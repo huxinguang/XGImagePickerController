4cd1e4046a260b8787402348d5ffbd8d8242ffcc
huxinguang <huxinguang@combplus.com>
2018-11-07 08:44:00 +0800

    fixed

--- XGImagePickerController/New Group/XGBarButton.m
+++ XGImagePickerController/New Group/XGBarButton.m
@@ -57,7 +57,6 @@
                 [self setTitleColor:configuration.disabledColor forState:UIControlStateDisabled];
             }
             break;
-            
         default:
             break;
     }
