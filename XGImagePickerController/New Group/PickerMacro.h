7e63013f57ea3d6abfc232eb87d1221f0236fe88
huxinguang <huxinguang@combplus.com>
2018-11-07 10:06:52 +0800

    适配iPhone XR/XS/XS_MAX

--- XGImagePickerController/New Group/PickerMacro.h
+++ XGImagePickerController/New Group/PickerMacro.h
@@ -12,12 +12,17 @@
 //=========================================通用宏==============================================
 #define kAppScreenWidth ([UIScreen mainScreen].bounds.size.width)//屏幕宽度
 #define kAppScreenHeight ([UIScreen mainScreen].bounds.size.height)//屏幕高度
-#define kAppStatusBarHeight (IS_iPhoneX ? 44.f : 20.f) // status bar height.
-#define kAppNavigationBarHeight 44.f //Navigation bar height.
-#define kAppTabbarHeight        (IS_iPhoneX ? (49.f+34.f) : 49.f)// Tabbar height.
-#define kAppTabbarSafeBottomMargin        (IS_iPhoneX ? 34.f : 0.f)// Tabbar safe bottom margin.
-#define kAppStatusBarAndNavigationBarHeight  (IS_iPhoneX ? 88.f : 64.f)// Status bar & navigation bar height.
-#define IS_iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)//判断是否iPhone X
+#define IS_Pad ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) //判断是否是ipad
+#define IS_iPhoneX_Or_Xs ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)//判断是否iPhone X/Xs
+#define IS_iPhoneXr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !IS_Pad : NO)//判断iPHoneXr
+#define IS_iPhoneXs_Max ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !IS_Pad : NO)//判断iPhoneXs Max
+#define IS_X_Series (IS_iPhoneX_Or_Xs || IS_iPhoneXr || IS_iPhoneXs_Max) //判断是否为带刘海的iPhone
+
+#define kAppStatusBarHeight ((IS_iPhoneX_Or_Xs || IS_iPhoneXr || IS_iPhoneXs_Max) ? 44.f : 20.f) //状态栏高度
+#define kAppNavigationBarHeight 44.f //导航栏高度（不包含状态栏）.
+#define kAppTabbarHeight ((IS_iPhoneX_Or_Xs || IS_iPhoneXr || IS_iPhoneXs_Max) ? (49.f+34.f) : 49.f)// Tabbar 高度.
+#define kAppTabbarSafeBottomMargin ((IS_iPhoneX_Or_Xs || IS_iPhoneXr || IS_iPhoneXs_Max) ? 34.f : 0.f)// Tabbar 底部安全高度.
+#define kAppStatusBarAndNavigationBarHeight ((IS_iPhoneX_Or_Xs || IS_iPhoneXr || IS_iPhoneXs_Max) ? 88.f : 64.f)// 状态栏和导航栏总高度.
 
 //=========================================导航栏(自定义部分)=====================================
 #define kAppNavigationTitleViewTitleFontSize 16
